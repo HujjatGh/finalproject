8c46d3a1b593c09f50fafcf3065a5361c4a130ad
HujjatGh <hujjat.pg@gmail.com>
2023-05-14 18:08:30 +0200

    The header done in template and style

--- src/App.vue
+++ src/App.vue
@@ -4,29 +4,22 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
+        <button id="appointment">SCHEDULE AN APPOINTMENT</button>
         <nav class="navigation-bar"></nav>
             <div class="container">
-            <a class="logo" href="/" alt="Carl's Dental Clinic"></a>
+            <img class="logo" src="https://media.istockphoto.com/id/1428043564/es/foto/icono-de-diente.jpg?s=612x612&w=0&k=20&c=Ku068NArjnoxQ43xjE3ESUIOAvOQCN_h8uWihplM4VM=" alt="Carl's Dental Clinic">
             <div class="navigation-pans">
                 <ul>
-                  <li class="nav-item">
-                    <a class="nav-link" href="/">WHY CARL</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="/">OUR SERVICES</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="/">THE TEAM</a> 
-                  </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="/">CONTACT US</a>
-                  </li>
+                  <li>WHY CARL</li> 
+                  <li>OUR SERVICES</li>
+                  <li>THE TEAM</li>
+                  <li>CONTACT US</li>
                 </ul>
               </div>
             </div>
     </header>
     <div class="patient-img">
-        <img src="" alt="Satisfied Patient">
+        <img src="https://media.istockphoto.com/id/1326436029/photo/happy-black-female-patient-looking-at-mirror-after-dental-treatment-in-clinic.jpg?b=1&s=170667a&w=0&k=20&c=7R1JQ3jtSdSSRSbfwWmSFlN09oen_Wthv4YGUXkORmc=" alt="Satisfied Patient">
     </div>
     <div>
         <p>HEAR FROM OUR PATIENTS</p>
@@ -49,16 +42,51 @@ import HelloWorld from './components/HelloWorld.vue'
 </template>
 
 <style scope>
+
+* {
+background-color: #E4C3AD;
+}
+
+#appointment {
+position: absolute;
+width: 248px;
+height: 52px;
+left: 1005px;
+top: 20px;
+border-radius: 11px;
+background: #0D1F2D;
+color: white;
+line-height: 0,5px;
+text-align: center;
+}
+
 .logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+position: absolute;
+width: 216px;
+height: 194px;
+left: 12px;
+top: 20px;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.navigation-pans {
+  list-style: none; 
+  display: flex; 
+  justify-content: space-between;
+  align-items: center; 
+  flex-direction: row;
+ 
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.navigation-pans li {
+  margin-right: 10px; 
+  background-color: #546A7B;
+}
+
+.patient-img {
+position: absolute;
+width: 836px;
+height: 331px;
+left: 222px;
+top: 205px;
 }
 </style>
